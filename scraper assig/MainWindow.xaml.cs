8137a3aa37051250785de40ad3b361c4ccf075e1
asad <asp@aptech.com>
2024-04-24 18:24:15 +0500

    assignment completed

---
+++ scraper assig/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+using Path = System.IO.Path;
+
+namespace scraper_assig
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_Copy_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void scrap_Click(object sender, RoutedEventArgs e)
+        {
+            WebClient client = new();
+            string reply = client.DownloadString(webURL.Text.ToString());
+            // Set a variable to the Documents path.
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            // Write the text to a new file named "WriteFile.txt".
+            File.WriteAllText(Path.Combine(docPath, webname.Text), reply);
+        }
+    }
+}
